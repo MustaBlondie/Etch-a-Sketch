989fe1ee5d5922c418ccdbbe7b8979d39dd9ad61
Maria Morales <victorina23x@gmail.com>
2024-08-22 11:37:00 -0400

    create css style sheet, add the styles to the div, input and button. create a number type input where the size of the grid is selected, use the button to send the value of the input to a function that generates the grid. add test hover effect to grid divs

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,20 @@
-let container = document.querySelector(".container");
-container.style.display = "flex";
-//container.style.width = "960px";
-//container.style.height = "300px"
+const container = document.querySelector(".container");
 
 let btn = document.querySelector(".btn");
-btn.style.textAlign = "center";
-btn.style.margin = "10px 10px";
 
-
-function divs(n){
-    for(let i = 0; i < n; i++){
-        const row = document.createElement("div");
-        for(let j = 0; j < n; j++){
-            const column = document.createElement('div');
-            column.style.width = "20px";
-            column.style.height = '20px';
-            column.style.border = "1px solid black";
-            row.appendChild(column);
-    
-        }
-        container.appendChild(row);
+function divs(n) {
+  for (let i = 0; i < n; i++) {
+    const row = document.createElement("div");
+    for (let j = 0; j < n; j++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
     }
+    container.appendChild(row);
+  }
 }
 
-btn.addEventListener("click", ()=> {
-    divs(n);
-})
+btn.addEventListener("click", () => {
+  const valores = document.getElementById("valores").value;
+  divs(valores);
+});
