8adaaa6eb08f22874145bc4d904c056129ca1f0f
Maria Morales <victorina23x@gmail.com>
2024-08-26 14:59:36 -0400

    add an addEventLister that detects when the mouse is in the parent container container adding the conditional if there is any container that contains the column class change the background color to black, this was the most far-fetched way to imitate a hover in js

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
-let btn = document.querySelector(".btn");
 
 function divs(n) {
+  container.innerHTML = "";
   for (let i = 0; i < n; i++) {
     const row = document.createElement("div");
     for (let j = 0; j < n; j++) {
@@ -11,10 +12,29 @@ function divs(n) {
       row.appendChild(column);
     }
     container.appendChild(row);
-  }
-}
+  };
+};
+
+function colors(){
+
+};
+
 
 btn.addEventListener("click", () => {
   const valores = document.getElementById("valores").value;
   divs(valores);
 });
+
+container.addEventListener("mouseover", function(event){
+  if(event.target.classList.contains('column')){
+    event.target.style.background = "black";
+  };
+});
+
+
+
+/*
+columnHover.addEventListener("mouseout", ()=> {
+  columnHover.style.transition = "background 4s";
+});
+*/
